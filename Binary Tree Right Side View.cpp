9fa3c1d3d97d681d479957439e1222fd33141871
Muhammad Altaweel <muhammadaltaweel.maids.cc@gmail.com>
2024-10-12 12:00:03 +0300

    Binary Tree Right Side View
    first solution by me

---
+++ Binary Tree Right Side View.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> sol;
+        solve(root, sol, 1);
+        return sol;
+    }
+    void solve(TreeNode* root, vector<int>& sol, int l) {
+        
+        if (!root)
+            return;
+        
+        if (sol.size() < l)
+            sol.push_back(root->val);
+        else
+            sol[l] = root->val;
+
+        cout << "______________________________________" << endl;
+        cout << "level: " << l << endl;
+        for (auto k : sol)
+            cout << k << " ";
+        cout << endl;
+        cout << "______________________________________" << endl;
+        
+        solve(root->left, sol, l + 1);
+        solve(root->right, sol, l + 1);
+    }
+};
